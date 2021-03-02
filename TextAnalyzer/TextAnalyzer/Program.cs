5e2831479f9148a5c22a8a72b98d0cf6c052d9d8
Nick Gallimore <nicholas.gallimore@rovisys.com>
2021-03-01 19:01:33 -0500

    Init

---
+++ TextAnalyzer/TextAnalyzer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace TextAnalyzer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string alice = TextAnalyzer.ReadFile("../../../../../Text1.txt");
+            string declaration = TextAnalyzer.ReadFile("../../../../../Text2.txt");
+
+            if (string.IsNullOrEmpty(alice) || string.IsNullOrEmpty(declaration))
+            {
+                Console.WriteLine($"File contents were empty or not found");
+                return;
+            }
+
+            Console.WriteLine(alice);
+        }
+    }
+}
---
+++ TextAnalyzer/TextAnalyzer/TextAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace TextAnalyzer
+{
+    public class TextAnalyzer
+    {
+        public static string ReadFile(string filepath)
+        {
+            try
+            {
+                string content = "";
+                using (StreamReader stringReader = File.OpenText(filepath))
+                {
+                    string line;
+                    while ((line = stringReader.ReadLine()) != null)
+                    {
+                        content += line;
+                    }
+                }
+
+                return content;
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"{filepath} was not found!");
+                return "";
+            }
+        }
+
+        public static string RemoveNonAlpha(string input)
+        {
+            return input;
+        }
+    }
+}
